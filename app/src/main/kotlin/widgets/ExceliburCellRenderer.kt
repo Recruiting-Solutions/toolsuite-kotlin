6592978fac387893a50af7c419636b3a3e9ad422
Boris Rosenow <brosenow@2orgu.com>
2023-09-05 13:46:24 +0200

    Converted ExceliburCellRenderer to Kotlin.

---
+++ app/src/main/kotlin/widgets/ExceliburCellRenderer.kt
@@ -0,0 +1,36 @@
+package widgets
+
+import java.awt.Color
+import java.awt.Component
+
+import javax.swing.JTable
+import javax.swing.table.DefaultTableCellRenderer
+import javax.swing.table.TableCellRenderer
+
+private val RENDERER: TableCellRenderer = DefaultTableCellRenderer()
+
+class ExceliburCellRenderer : TableCellRenderer {
+
+    override fun getTableCellRendererComponent(table: JTable?, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component {
+        val c = RENDERER.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+        val obj = table?.getValueAt(row, column)?.toString()
+
+        when {
+            obj.isNullOrBlank() -> {
+                c.setBackground(if (isSelected) Color(255, 192, 192) else Color(255, 110, 110))
+            }
+
+            column < 2 -> {
+                c.setBackground(if (isSelected) Color(57, 135, 213) else Color(255, 255, 255, 32))
+                c.setForeground(if (isSelected) Color(255, 255, 255) else Color(187, 187, 187, 128))
+            }
+
+            else -> {
+                c.setBackground(if (isSelected) Color(57, 135, 213) else Color(0, 0, 0, 0))
+                c.setForeground(if (isSelected) Color(255, 255, 255) else Color(187, 187, 187))
+            }
+        }
+
+        return c
+    }
+}
