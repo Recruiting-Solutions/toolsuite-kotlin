be99d384b46c8dd6e0f602467d3e07dcddc47c3f
Boris Rosenow <brosenow@2orgu.com>
2023-09-05 16:54:31 +0200

    Converted LanguageTable to Kotlin.

---
+++ app/src/main/kotlin/structs/LanguageTable.kt
@@ -0,0 +1,12 @@
+package structs
+
+class LanguageTable(val identifiers: Array<LanguageIdentifier>, val jTableData: Array<Array<String>>) {
+
+    fun getIdentifierIndex(otherIdentifier: LanguageIdentifier): Int {
+        return identifiers.indexOfFirst { it.brand == otherIdentifier.brand && it.locale == otherIdentifier.locale }
+    }
+
+    fun getJTableHeader(): Array<String> {
+        return arrayOf("Component", "Key", *identifiers.map { it.locale }.toTypedArray())
+    }
+}
