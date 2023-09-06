dbbb91f945e0157e2eb3d5a9ad007c505ed77c03
Boris Rosenow <brosenow@2orgu.com>
2023-09-06 10:34:28 +0200

    Converted MainMenu to Kotlin.

---
+++ app/src/main/kotlin/widgets/MainMenu.kt
@@ -0,0 +1,40 @@
+package widgets
+
+import java.awt.Dimension
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import java.awt.Insets
+
+import javax.swing.JButton
+import javax.swing.JPanel
+
+import core.App
+
+class MainMenu(owner: App) : JPanel() {
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
+    init {
+        val grid = GridBagLayout()
+        setLayout(grid)
+        val con = GridBagConstraints().apply {
+            gridx = 0
+            gridy = 0
+            insets = Insets(8, 0, 8, 0)
+        }
+        val button = App.createButtonWithTextAndIcon("Excelibur", "icon_excel.png").apply {
+            addActionListener { owner.addScreen(Excelibur(owner), "Excelibur") }
+            preferredSize = Dimension(200, 50)
+        }
+        add(button, con);
+        con.gridy = 1;
+        val helpButton = App.createButtonWithTextAndIcon("Documentation", "icon_help.png").apply {
+
+            addActionListener { owner.showDocumentation() }
+            preferredSize = Dimension(200, 50)
+        }
+        add(helpButton, con)
+        owner.setStatus("Welcome to the Toolsuite...", App.NORMAL_MESSAGE)
+    }
+}
